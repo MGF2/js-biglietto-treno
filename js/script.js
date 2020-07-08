d42b5f3e951a7eb8f7f666f69413552e23a77a90
Unknown <fioremartina@hotmail.com>
2020-07-08 10:05:51 +0200

    ok

--- js/script.js
+++ js/script.js
@@ -1,19 +1,16 @@
 var km = parseInt(prompt("Che distanza devi percorrere?")) ;
 var eta = parseInt(prompt("Quanti anni hai?")) ;
 var prezzo = 0.21 * km ;
-var venti = prezzo * 0.8;
-var quaranta = prezzo * 0.6;
-
 
 
 if  (eta < 18)  {
 
-  prezzo = venti
+  prezzo = prezzo * 0.8;
 
 }
 else if (eta > 65) {
 
-  prezzo = quaranta
+  prezzo = prezzo * 0.6;
 
 }
 
